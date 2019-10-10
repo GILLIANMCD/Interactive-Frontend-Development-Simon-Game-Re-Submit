fc1af40a76a43bee01671f129316d11e6a6b85ba
GILLIANMCD <51652977+GILLIANMCD@users.noreply.github.com>
2019-10-10 16:54:55 +0100

    Event Listener added to turn on score counter when onOff Button Clicked

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,24 @@
 // Variables
-const onOff = document.getElementById("switch");
+let on = false;
 
+const onOff = document.getElementById("switch");
+const score = document.getElementById("score");
 
 //Event Listeners
-onOff.addEventListener("click", turnOn);
+onOff.addEventListener("click", (turnOn) => {
+    if (onOff.checked == true) {
+        on = true;
+        score.innerHTML ="--";
+    } else {
+        on = false;
+        score.innerHTML = "";
+
+    }
+});
 
 
 
 // Functions
 
-function turnOn(){
- alert ("Hello World");  
 
-    }
 
