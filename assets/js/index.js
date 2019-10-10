3e22434219805fca2e920e0f4802e7d9d4a3892a
GILLIANMCD <51652977+GILLIANMCD@users.noreply.github.com>
2019-10-10 18:04:04 +0100

    strict button changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,28 @@
 // Variables
 let on = false;
+let strict = false;
+
 
 const onOff = document.getElementById("switch");
 const score = document.getElementById("score");
+const strict = document.getElementById("strict");
 
 //Event Listeners
-onOff.addEventListener("click", (turnOn) => {
+onOff.addEventListener("click", () => {
     if (onOff.checked == true) {
         on = true;
         score.innerHTML ="--";
     } else {
         on = false;
         score.innerHTML = "";
-
     }
 });
 
-
-
-// Functions
-
-
-
+strict.addEventListener("click", () => {
+   if (strict.checked == true) {
+       strict = true;
+    } else {
+        strict = false;
+    }
+    });
+    
