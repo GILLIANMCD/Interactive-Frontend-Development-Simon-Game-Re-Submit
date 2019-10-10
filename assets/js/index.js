836bf039878116dfb9a2da98d2f3d73b4962057a
GILLIANMCD <51652977+GILLIANMCD@users.noreply.github.com>
2019-10-10 16:32:01 +0100

    first javascript test of event listener

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// Variables
+const onOff = document.getElementById("switch");
+
+
+//Event Listeners
+onOff.addEventListener("click", turnOn);
+
+
+
+// Functions
+
+function turnOn(){
+ alert ("Hello World");  
+
+    }
+
