2072e94bb20f7767e3c162c55cec5cdb1986292b
GILLIANMCD <51652977+GILLIANMCD@users.noreply.github.com>
2019-10-18 11:00:37 +0100

    jQuery for inner circle buttons to change on activation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,32 @@
 // Variables
-let on = false;
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn
+let intervalId;
 let strict = false;
+let noise = true;
+let on = false;
+let win;
 
 
-const onOff = document.getElementById("switch");
+const onOffButton = document.getElementById("switch");
+const strictButton = document.getElementById("strict");
+const startButton = document.getElementById("start");
 const score = document.getElementById("score");
-const strict = document.getElementById("strict");
+const green = document.getElementsByClassName("green");
+const red = document.getElementsByClassName("red");
+const blue = document.getElementsByClassName("blue");
+const yellow = document.getElementsByClassName("yellow");
 
 //Event Listeners
-onOff.addEventListener("click", () => {
-    if (onOff.checked == true) {
+
+//Turn Game on to activate the Score 
+
+onOffButton.addEventListener("click", () => {
+   if (onOffButton.checked == true) {
         on = true;
         score.innerHTML ="--";
     } else {
@@ -18,11 +35,37 @@ onOff.addEventListener("click", () => {
     }
 });
 
-strict.addEventListener("click", () => {
-   if (strict.checked == true) {
-       strict = true;
+//Strict Button resets the game if player has incorrect move 
+
+/*strictButton.addEventListener('click', (event) => {
+    if (strictButton.checked == true){
+        strict = true;
     } else {
-        strict = false;
+        strict = false; 
     }
-    });
+});*/
+
+$(document).ready(function(){ //jQuery & javaScript when the DOM is ready
+
+//Turns Strict Mode ON/OFF -- Does Not Turn Off ---
+$('#strict').on('click', function() {
+    $('#strict').removeClass('selected');
+    $(this).addClass('selected');
+});
+$('#strict').off('click', function() {
+    $('#strict').removeClass('selected');
+    $(this).addClass('#strict');
+});
+
+$('#start').on('click', function() {
+    $('#start').removeClass('selected');
+    $(this).addClass('selected');
+});
+
+});
+
+
+
+
+
     
