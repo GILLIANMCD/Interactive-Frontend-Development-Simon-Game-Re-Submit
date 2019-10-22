57536a60b1230136030cf97d3898369d6cd11500
GILLIANMCD <51652977+GILLIANMCD@users.noreply.github.com>
2019-10-22 11:17:25 +0100

    play function variable to console log strict and start changed to checkbox

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,17 @@
 // Variables
-let order = [];
-let playerOrder = [];
+let order = []; 
+let playerOrder = []; 
 let flash;
 let turn;
 let good;
-let compTurn
+let compTurn;
 let intervalId;
 let strict = false;
 let noise = true;
 let on = false;
 let win;
 
-
+//Game Buttons
 const onOffButton = document.getElementById("switch");
 const strictButton = document.getElementById("strict");
 const startButton = document.getElementById("start");
@@ -31,26 +31,50 @@ onOffButton.addEventListener("click", () => {
         score.innerHTML ="--";
     } else {
         on = false;
+        strict = false;
         score.innerHTML = "";
+        clearInterval(intervalId);
     }
 });
 
-$(document).ready(function(){ //jQuery & javaScript when the DOM is ready
-
 /*Turns Strict Mode ON/OFF*/
-$(strictButton).on('click', function() {
-    if (strict.selected = false)
 
-    $(this).addClass('selected');
+$(strictButton).on("click", function() {
+    if (strictButton.checked == true) {
+     strict = true;
+     console.log("strict true")
+     }else {
+         strict = false;
+        console.log("strict false");
+     }
+ });
 
-    else (strict = false)
-    $(this).removeClass('selected')
-    });
-
- 
-$('#start').on('click', function() {
-    $('#start').removeClass('selected');
-    $(this).addClass('selected');
+ // Play the Game only if onOffButton  is true otherwise alert the player on button is not clicked
+$(startButton).on('click', function() {
+    if (on || win) {
+        clearInterval(intervalId);
+        play();
+        console.log("start true")
+    } else {
+        on = false;
+        alert("Please Turn the Game on to Play")
+    }
 });
 
-});
+function play () {
+    win = false;
+    order = [];
+    flash = 0;
+    turn = 1;
+    score.innerHTML = 1;
+    good = true;
+    for (var i = 0; i < 20; i++ ) {
+        order.push(Math.floor(Math.random() * 4) + 1);
+    }
+    console.log(order)
+    compTurn = true;
+ }
+
+
+
+
