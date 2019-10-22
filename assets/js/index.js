6cb44830ad49dee6dba9a0af3b7b3049fc316c64
GILLIANMCD <51652977+GILLIANMCD@users.noreply.github.com>
2019-10-22 15:44:51 +0100

    falsh colors added

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,15 +11,21 @@ let noise = true;
 let on = false;
 let win;
 
+// Audio for Game Buttons 
+let greenSound = new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+let redSound = new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+let yellowSound = new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+let blueSound = new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+
 //Game Buttons
 const onOffButton = document.getElementById("switch");
 const strictButton = document.getElementById("strict");
 const startButton = document.getElementById("start");
 const score = document.getElementById("score");
-const green = document.getElementsByClassName("green");
-const red = document.getElementsByClassName("red");
-const blue = document.getElementsByClassName("blue");
-const yellow = document.getElementsByClassName("yellow");
+const greenQuad = document.getElementsByClassName("green");
+const redQuad = document.getElementsByClassName("red");
+const blueQuad = document.getElementsByClassName("blue");
+const yellowQuad = document.getElementsByClassName("yellow");
 
 //Event Listeners
 
@@ -42,6 +48,7 @@ onOffButton.addEventListener("click", () => {
 $(strictButton).on("click", function() {
     if (strictButton.checked == true) {
      strict = true;
+     flashColor();
      console.log("strict true")
      }else {
          strict = false;
@@ -52,7 +59,6 @@ $(strictButton).on("click", function() {
  // Play the Game only if onOffButton  is true otherwise alert the player on button is not clicked
 $(startButton).on('click', function() {
     if (on || win) {
-        clearInterval(intervalId);
         play();
         console.log("start true")
     } else {
@@ -61,20 +67,40 @@ $(startButton).on('click', function() {
     }
 });
 
-function play () {
+function play() {
     win = false;
     order = [];
+    playerOrder = [];
     flash = 0;
+    intervalId = 0;
     turn = 1;
     score.innerHTML = 1;
     good = true;
-    for (var i = 0; i < 20; i++ ) {
-        order.push(Math.floor(Math.random() * 4) + 1);
+    for (var i = 0; i < 20; i++) {
+      order.push(Math.floor(Math.random() * 4) + 1);
     }
-    console.log(order)
+    console.log(order);
     compTurn = true;
- }
+  
+  }
 
+
+
+// Active and inActive Colors 
+function clearColor() {
+    $(green).removeClass("green-active");
+    $(red).removeClass("red-active");
+    $(yellow).removeClass("yellow-active");
+    $(blue).removeClass("blue-active");
+}
+
+function flashColor () {
+    $(green).addClass("green-active");
+    $(red).addClass("red-active");
+    $(yellow).addClass("yellow-active");
+    $(blue).addClass("blue-active");
+
+}
 
 
 
