3896b2edd3e4b97776fb40ea4a84d77062e68a5e
GILLIANMCD <51652977+GILLIANMCD@users.noreply.github.com>
2019-10-24 13:28:23 +0100

    javascript complete game working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 // Variables
-let order = []; 
-let playerOrder = []; 
+let order = []; //random generated sequence
+let playerOrder = []; //
 let flash;
 let turn;
 let good;
@@ -34,11 +34,14 @@ const yellowQuad = document.getElementsByClassName("yellow");
 onOffButton.addEventListener("click", () => {
    if (onOffButton.checked == true) {
         on = true;
+        clearColor();
         score.innerHTML ="--";
     } else {
         on = false;
-        strict = false;
+        strictButton.checked = false;
+        startButton.checked = false;
         score.innerHTML = "";
+        clearColor();
         clearInterval(intervalId);
     }
 });
@@ -48,11 +51,11 @@ onOffButton.addEventListener("click", () => {
 $(strictButton).on("click", function() {
     if (strictButton.checked == true) {
      strict = true;
-     flashColor();
      console.log("strict true")
      }else {
          strict = false;
-        console.log("strict false");
+         strictButton.checked = false;
+         console.log("strict false");
      }
  });
 
@@ -63,6 +66,7 @@ $(startButton).on('click', function() {
         console.log("start true")
     } else {
         on = false;
+        startButton.checked = false;
         alert("Please Turn the Game on to Play")
     }
 });
@@ -70,21 +74,67 @@ $(startButton).on('click', function() {
 function play() {
     win = false;
     order = [];
+    randomMoves();
     playerOrder = [];
     flash = 0;
     intervalId = 0;
     turn = 1;
     score.innerHTML = 1;
     good = true;
-    for (var i = 0; i < 20; i++) {
-      order.push(Math.floor(Math.random() * 4) + 1);
-    }
-    console.log(order);
     compTurn = true;
+    intervalId = setInterval(compPlay, 800);
+    }
+//Generates Computer Turn sequence randomly
+function randomMoves () {
+    for (var i = 0; i < 20; i++) {
+        order.push(Math.floor(Math.random() * 4) + 1);
+      }
+}
+
+//compPlay play from freeCodeCamp.org
+function compPlay() {
+    on = false; // player cannot click buttons when on is = to false
   
+    if (flash == turn) {
+      clearInterval(intervalId);
+      compTurn = false;
+      clearColor();
+      on = true;
+    }
+  
+    if (compTurn) {
+      clearColor();
+      setTimeout(() => {
+        if (order[flash] == 1) one();
+        if (order[flash] == 2) two();
+        if (order[flash] == 3) three();
+        if (order[flash] == 4) four();
+        flash++;
+      }, 200);
+    }
   }
 
 
+// Play sound and activate light
+function one() {
+    greenSound.play();
+    $(green).addClass("green-active");
+}
+
+function two() {
+    redSound.play();
+    $(red).addClass("red-active");
+}
+
+function three() {
+    yellowSound.play();
+    $(yellow).addClass("yellow-active");
+}
+
+function four() {
+    blueSound.play();
+    $(blue).addClass("blue-active");
+}
 
 // Active and inActive Colors 
 function clearColor() {
@@ -102,5 +152,103 @@ function flashColor () {
 
 }
 
+$(green).on("click", function(){
+    if (on) {
+        playerOrder.push(1);
+        check();
+        one();
+    if (!win) {
+        setTimeout(() => {
+            clearColor();
+        }, 300);
+    }   
+   }
+});
 
+$(red).on("click", function(){
+    if (on) {
+        playerOrder.push(2);
+        check();
+        two();
+    if (!win) {
+        setTimeout(() => {
+            clearColor();
+        }, 300);
+    }   
+   }
+});
 
+$(yellow).on("click", function(){
+    if (on) {
+        playerOrder.push(3);
+        check();
+        three();
+    if (!win) {
+        setTimeout(() => {
+            clearColor();
+        }, 300);
+    }   
+   }
+});
+
+$(blue).on("click", function(){
+    if (on) {
+        playerOrder.push(4);
+        check();
+        four();
+    if (!win) {
+        setTimeout(() => {
+            clearColor();
+        }, 300);
+    }   
+   }
+});
+
+//check if playerOrder matches play freecodecamp.org
+function check() {
+    if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1])
+      good = false;
+  
+    if (playerOrder.length == 20 && good) {
+      winGame();
+    }
+  
+    if (good == false) {
+      flashColor();
+      score.innerHTML = "NO!";
+      setTimeout(() => {
+       score.innerHTML = turn;
+        clearColor();
+  
+        if (strict) {
+          play();
+        } else {
+          compTurn = true;
+          flash = 0;
+          playerOrder = [];
+          good = true;
+          intervalId = setInterval(compPlay, 800);
+        }
+      }, 800);
+  
+      noise = false;
+    }
+  
+    if (turn == playerOrder.length && good && !win) {
+      turn++;
+      playerOrder = [];
+      compTurn = true;
+      flash = 0;
+      score.innerHTML = turn;
+      intervalId = setInterval(compPlay, 800);
+    }
+  
+  }
+  
+  function winGame() {
+    flashColor();
+    score.innerHTML = "WIN!";
+    on = false;
+    win = true;
+  }
+  
